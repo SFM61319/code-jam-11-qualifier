497f63f44ead127c762b794072ba50cb08641986
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2024-07-04 10:26:35 +0530

    Chore: Clean-up `qualifier` module

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -4,59 +4,96 @@ from enum import auto, StrEnum
 MAX_QUOTE_LENGTH = 50
 
 
-# The two classes below are available for you to use
-# You do not need to implement them
 class VariantMode(StrEnum):
+    """
+    Enum representing the different modes that a quote can be transformed into.
+
+    - `NORMAL`: The quote is returned as-is, without any transformation.
+    - `UWU`: The quote is transformed into the "UwU" style.
+    - `PIGLATIN`: The quote is transformed into Pig Latin.
+    """
+
     NORMAL = auto()
     UWU = auto()
     PIGLATIN = auto()
 
 
 class DuplicateError(Exception):
-    """Error raised when there is an attempt to add a duplicate entry to a database"""
+    """
+    Error raised when there is an attempt to add a duplicate entry to a database
+    """
 
 
-# Implement the class and function below
 class Quote:
-    def __init__(self, quote: str, mode: "VariantMode") -> None:
-        self.quote = ...
-        self.mode = ...
+    """
+    Represents a quote with a specified transformation mode.
+
+    The `Quote` class encapsulates a quote string and a `VariantMode`
+    that determines how the quote should be transformed. The
+    `_create_variant()` method is responsible for applying the
+    appropriate transformation to the quote based on the specified mode.
+    """
+
+    quote: str
+    mode: VariantMode
+
+    def __init__(self, quote: str, mode: VariantMode) -> None:
+        self.quote = quote
+        self.mode = mode
 
     def __str__(self) -> str: ...
 
     def _create_variant(self) -> str:
         """
-        Transforms the quote to the appropriate variant indicated by `self.mode` and returns the result
+        Transforms `self.quote` to the appropriate variant indicated by
+        `self.mode` and returns the result.
         """
 
 
 def run_command(command: str) -> None:
     """
-    Will be given a command from a user. The command will be parsed and executed appropriately.
+    Will be given a command from a user. The command will be parsed and
+    executed appropriately.
 
     Current supported commands:
-        - `quote` - creates and adds a new quote
-        - `quote uwu` - uwu-ifys the new quote and then adds it
-        - `quote piglatin` - piglatin-ifys the new quote and then adds it
-        - `quote list` - print a formatted string that lists the current
+        - `quote`: creates and adds a new quote
+        - `quote uwu`: uwu-ifys the new quote and then adds it
+        - `quote piglatin`: piglatin-ifys the new quote and then adds it
+        - `quote list`: print a formatted string that lists the current
            quotes to be displayed in discord flavored markdown
     """
-    ...
 
 
-# The code below is available for you to use
-# You do not need to implement it, you can assume it will work as specified
 class Database:
-    quotes: list["Quote"] = []
+    """
+    Provides a simple in-memory database for storing and retrieving
+    `Quote` objects.
+
+    The `Database` class manages a list of `Quote` objects, providing methods
+    to add new quotes and retrieve the current list of quotes.
+
+    If an attempt is made to add a duplicate quote
+    (based on the string representation of the `Quote`
+    object), a `DuplicateError` exception will be raised.
+    """
+
+    quotes: list[Quote] = []
 
     @classmethod
     def get_quotes(cls) -> list[str]:
-        "Returns current quotes in a list"
+        """
+        Returns current quotes in a list.
+        """
+
         return [str(quote) for quote in cls.quotes]
 
     @classmethod
-    def add_quote(cls, quote: "Quote") -> None:
-        "Adds a quote. Will raise a `DuplicateError` if an error occurs."
-        if str(quote) in [str(quote) for quote in cls.quotes]:
+    def add_quote(cls, quote: Quote) -> None:
+        """
+        Adds a quote. Will raise a `DuplicateError` if an error occurs.
+        """
+
+        if str(quote) in cls.get_quotes():
             raise DuplicateError
+
         cls.quotes.append(quote)
